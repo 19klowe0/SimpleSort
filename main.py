8a87cd7b1bcbe8445f754a968ec34bc9c814319c
19klowe0 <kiralowe98@gmail.com>
2021-03-15 18:21:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ names.sort(key=len)
 for name in names:
     print(name)
 
+print("_____________ Reverse It")
+print("")
 
+#code for reversing
 
+names.sort(reverse=True)
+names.sort(key=len, reverse=True)
+
+for name in names:
+    print(name)
